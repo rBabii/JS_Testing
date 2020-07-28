36c2fed9e5560b44fee883caee05ac6fae06b9a2
rBabii <romababjjm@gmail.com>
2020-07-28 13:00:03 +0300

    add a calc logic

--- script.js
+++ script.js
@@ -1,14 +1,49 @@
 const clickElement_Wrapper = document.querySelector("#main-wrapper");
+const resoult = document.querySelector("#resoult");
+const allInputs = document.querySelectorAll(".inp-style");
+
+
 
 let mainClick = (event) => {
-if(event.target.parentElement.classList.contains("btn-inp-wrapper")){
-   let parElem = event.target.parentElement;
-   let inpElement = parElem.querySelector(".inp-style");
-   let btnElement = parElem.querySelector(".btn-style");
-   console.log(btnElement, inpElement);
+   let sumOfInpValue = 0;
+if(event.type === "click") {
+   if(event.target.parentElement.classList.contains("btn-inp-wrapper")){
+      let parElem = event.target.parentElement;
+      let inpElement = parElem.querySelector(".inp-style");
+         if (event.target.classList.contains("btn-style")){
+            if (event.target.classList.contains("active")){
+               event.target.classList.remove("active");
+               document.querySelector(".container").click();
+               inpElement.value = "";
+            }else{
+               event.target.classList.add("active");
+               inpElement.focus();
+            }
+            
+         }
+   }
+   
+
+}else if (event.type === "input"){
+   if(event.target.parentElement.classList.contains("btn-inp-wrapper")){
+      let parElem = event.target.parentElement;
+      let btnElement = parElem.querySelector(".btn-style");
+   }
 }
-}
+
+
+allInputs.forEach( inputElement => {
+  sumOfInpValue += Number(inputElement.value);
+})
+
+resoult.innerHTML=sumOfInpValue;
+
+}//end func
+
+
+
 
 clickElement_Wrapper.addEventListener("click", mainClick);
 
+clickElement_Wrapper.addEventListener("input", mainClick);
 
